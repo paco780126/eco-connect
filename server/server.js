712aad4cac6ddf3fe51584714abb56a054ba10b6
Choi In-gi <chl7dls8rl78@gmail.com>
2025-08-01 04:52:14 +0900

    fix(auth): Restore missing functions in AuthContext

--- server/server.js
+++ server/server.js
@@ -10,8 +10,8 @@ const app = express();
 // Vercel에서 만들어질 우리 집 주소들을 미리 다 알려주는 거야.
 const corsOptions = {
   origin: [
-    'https://eco-connect-client-jisu.vercel.app', // client 주소
-    'https://eco-connect-admin-jisu.vercel.app'  // admin 주소 (예상)
+    'https://eco-connect-client-jisu.vercel.app', 
+    'https://eco-connect-admin.vercel.app' 
   ],
   optionsSuccessStatus: 200
 };
